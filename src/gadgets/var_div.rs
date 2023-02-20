c049ba31c2c855bff83bbc70b0f7f4a395544754
Daniel Kang <daniel.d.kang@gmail.com>
2023-02-20 11:38:13 -0800

    use rounded div?

--- src/gadgets/var_div.rs
+++ src/gadgets/var_div.rs
@@ -6,6 +6,7 @@ use halo2_proofs::{
   plonk::{ConstraintSystem, Error, Expression},
   poly::Rotation,
 };
+use rounded_div::RoundedDiv;
 
 use super::gadget::{convert_to_u128, Gadget, GadgetConfig, GadgetType, USE_SELECTORS};
 
@@ -156,7 +157,8 @@ impl<F: FieldExt> Gadget<F> for VarDivRoundChip<F> {
         let a_pos = *a + div_inp_min_val_pos;
         let a = convert_to_u128(&a_pos);
         let b = convert_to_u128(b);
-        let c = ((2 * a + b) / (2 * b)) - (div_inp_min_val_pos_i64 as u128 / b);
+        let c = a.rounded_div(b) - (div_inp_min_val_pos_i64 as u128 / b);
+        // let c = ((2 * a + b) / (2 * b)) - (div_inp_min_val_pos_i64 as u128 / b);
         let c = c as i64; // The intermediate can go to u128, but the result must be in i64
         let r = (2 * a + b) % (2 * b);
         let r = r as i64;
