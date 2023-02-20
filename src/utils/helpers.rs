fa7c7d43a688f8e8f70ff26189d956f0f57c753d
Daniel Kang <daniel.d.kang@gmail.com>
2023-02-20 14:13:22 -0800

    fix printing hopefully

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -4,14 +4,14 @@ use halo2_proofs::{
 };
 use ndarray::{Array, IxDyn};
 
-use crate::gadgets::gadget::convert_to_u64;
+use crate::gadgets::gadget::convert_to_u128;
 
 pub fn print_pos_int<F: FieldExt>(prefix: &str, x: Value<F>) {
-  let bias = 1 << 40;
+  let bias = 1 << 60;
   let x_pos = x + Value::known(F::from(bias as u64));
   x_pos.map(|x| {
-    let x_pos = convert_to_u64(&x);
-    let tmp = x_pos as i64 - bias;
+    let x_pos = convert_to_u128(&x);
+    let tmp = x_pos as i128 - bias;
     println!("{} x: {}", prefix, tmp);
   });
 }
