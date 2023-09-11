5c5160fd8b3622c20366a6bcb528d7897f26ec10
span14 <span14@illinois.edu>
2023-09-11 12:51:33 -0500

    update cargo build

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(int_roundings)]
 
 pub mod commitments;
-pub mod conversion;
+// pub mod conversion;
 pub mod gadgets;
 pub mod layers;
 pub mod model;
 pub mod utils;
-pub mod simple;
+// pub mod simple;
